53d6e2ffd01f79d7a1aeec4aafe7b0e71e0d28a7
Prateek Sahay <prateeksahay96@gmail.com>
2018-09-17 11:41:02 +0530

    Delegate

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -2,13 +2,15 @@
 using System.Linq;
 using System.Collections.Generic;
 
-namespace Transform
-{
-    public static class Transform 
-    {
-        public static void Map()
-        {
-            throw new NotImplementedException();
+
+namespace Transform{
+    public delegate int Sqre(int x);
+    public static class Transform {
+        public static int[] Map(this int[] a,Sqre sqr){
+            for (int i = 0; i < a.Length; i++){
+                a[i]=sqr(a[i]); 
+            }
+            return a;
         }
     }
 }
